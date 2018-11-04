9128b2b2cc6232dd40daef71503c4874735b3d48
Gabriela Croce <gabycroce@gmail.com>
2018-11-04 18:38:21 +0100

    User parameterized tests

--- tests/it/tradingbots/lob/OrderBookTest.java
+++ tests/it/tradingbots/lob/OrderBookTest.java
@@ -1,61 +1,59 @@
 package it.tradingbots.lob;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class OrderBookTest {
 
-	@Test
-	void testFirstOrderFromTop() {
+	@ParameterizedTest
+	@ValueSource(strings = {"true", "false"})
+	void testFirstOrder(boolean fromTop) {
 		OrderBook book = new OrderBook(50);
-		book.bid(50,  1000, true);
+		book.bid(50,  1000, fromTop);
 		assertIterableEquals(Arrays.asList(new PriceLevel(50, 1000)), book.getBids(0));
 	}
 
-	@Test
-	void testFirstOrderFromBottom() {
+	@ParameterizedTest
+	@ValueSource(strings = {"true", "false"})
+	void testSeveralNewOrders(boolean fromTop) {
 		OrderBook book = new OrderBook(50);
-		book.bid(50,  1000, false);
-		assertIterableEquals(Arrays.asList(new PriceLevel(50, 1000)), book.getBids(0));
-	}
-
-	@Test
-	void testSeveralNewOrdersFromTop() {
-		OrderBook book = new OrderBook(50);
-		book.bid(50,  1000, true);
-		book.bid(70, 1500, true);
-		book.bid(60, 500, true);
+		book.bid(50,  1000, fromTop);
+		book.bid(70, 1500, fromTop);
+		book.bid(60, 500, fromTop);
 		assertIterableEquals(Arrays.asList(
 				new PriceLevel(70, 1500),
 				new PriceLevel(60, 500),
 				new PriceLevel(50, 1000)), book.getBids(0));
 	}
 	
-	@Test
-	void testSeveralNewOrdersFromTopSorted() {
+	@ParameterizedTest
+	@ValueSource(strings = {"true", "false"})
+	void testSeveralNewOrdersSorted(boolean fromTop) {
 		OrderBook book = new OrderBook(50);
-		book.bid(70,  1000, true);
-		book.bid(60, 1500, true);
-		book.bid(50, 500, true);
+		book.bid(70,  1000, fromTop);
+		book.bid(60, 1500, fromTop);
+		book.bid(50, 500, fromTop);
 		assertIterableEquals(Arrays.asList(
 				new PriceLevel(70, 1000),
 				new PriceLevel(60, 1500),
 				new PriceLevel(50, 500)), book.getBids(0));
 	}
 	
-	@Test
-	void testSeveralNewOrdersFromBottom() {
+	@ParameterizedTest
+	@ValueSource(strings = {"true", "false"})
+	void testSeveralNewOrdersReverseSorted(boolean fromTop) {
 		OrderBook book = new OrderBook(50);
-		book.bid(50,  1000, false);
-		book.bid(70, 1500, false);
-		book.bid(60, 500, false);
+		book.bid(50,  1000, fromTop);
+		book.bid(60, 1500, fromTop);
+		book.bid(70, 500, fromTop);
 		assertIterableEquals(Arrays.asList(
-				new PriceLevel(70, 1500),
-				new PriceLevel(60, 500),
+				new PriceLevel(70, 500),
+				new PriceLevel(60, 1500),
 				new PriceLevel(50, 1000)), book.getBids(0));
 	}
 }
